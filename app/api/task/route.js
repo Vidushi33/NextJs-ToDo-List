14f73c508a81c74c707c339aa9515a4826bca032
Vidushi Malik <vidushimalik135@gmail.com>
2023-10-26 14:37:48 +0530

    New Commit

--- app/api/task/route.js
+++ app/api/task/route.js
@@ -2,6 +2,28 @@ import ConnectToMongoDb from "@/lib/mongodb";
 import Task from "@/models/task";
 import { NextResponse } from "next/server";
 
+export default async (req, res) => {
+    // Enable CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your actual origin or origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, DELETE, PATCH, POST, PUT');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+  
+    // Specific logic for the preflight request
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+  
+    // Your API route logic here
+    const currentDate = new Date();
+    res.status(200).json({ timestamp: currentDate.getTime(), message: 'API response' });
+  };
+  
+
 
 export async function POST(req){
     const {title, description} = await req.json()
