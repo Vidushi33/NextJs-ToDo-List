d1c280a075fadb7c949d5042974b7259cd3b7476
Vidushi Malik <vidushimalik135@gmail.com>
2024-01-12 11:42:23 +0530

    chnages made

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,14 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
 import AddTaskModal from "./addtaskModal";
 
+
 const Navbar = () => {
+  
+
+
   return (
     <nav className="max-w-3xl mx-auto p-4 bg-slate-800 flex justify-between items-center">
       <Link href="/" className="text-white font-bold text-xl">
@@ -11,8 +17,11 @@ const Navbar = () => {
       <Link href="/" className="bg-white p-2 font-bold text-xl ">
         <AddTaskModal />
       </Link>
+    
     </nav>
   );
 };
+
+
 
 export default Navbar;
